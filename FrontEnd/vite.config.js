7108f2c4c56ba75f149192349e686cbf7429eaf9
sahilali88084667@gmail.com <sahilali88084667@gmail.com>
2024-03-27 21:38:36 +0530

    env configured

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import dotenv from 'dotenv';
 import react from '@vitejs/plugin-react'
-
+dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': process.env
+  }
 })
