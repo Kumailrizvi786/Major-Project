8daac82f857c0a43c5a38d0e8e0ef8bdac290cc7
sahilali88084667@gmail.com <sahilali88084667@gmail.com>
2024-03-29 17:47:52 +0530

    leader board implemented

--- FrontEnd/src/pages/Leaderboard.jsx
+++ FrontEnd/src/pages/Leaderboard.jsx
@@ -1,11 +1,107 @@
-import React from 'react'
+import React from 'react';
+import Tables from '../components/Tables';
+import { SegmentedControl } from '@radix-ui/themes';
+import { Box, Card, Flex, Avatar, Text } from '@radix-ui/themes';
+import { RiAwardLine } from 'react-icons/ri';
+import { FaTrophy } from 'react-icons/fa';
+import Breadcrumbs from '../components/Breadcrumb';
+import { IoHomeOutline } from 'react-icons/io5';
+function LeaderboardList() {
 
-function Leaderboard() {
+  const data = [
+    { rank: 1, name: 'Mohd Maaz', speed: '250 w/m' },
+    { rank: 2, name: 'Syed Kumail Rizvi', speed: '248 w/m' },
+    { rank: 3, name: 'Sahil Ali', speed: '238 w/m' },
+    { rank: 4, name: 'Lucky Ali', speed: '235 w/m' },
+    { rank: 5, name: 'Ayesha Siddiqua', speed: '230 w/m' },
+    { rank: 6, name: 'Sana Fatima', speed: '225 w/m' },
+    { rank: 7, name: 'Zeba Shaikh', speed: '220 w/m' },
+    { rank: 8, name: 'Mohd Owais', speed: '215 w/m' },
+    { rank: 9, name: 'Mohd Zaid', speed: '210 w/m' },
+    { rank: 10, name: 'Mohd Abdullah', speed: '205 w/m' },
+  ]
+  const breadcrumbs = [
+    { label: 'Home', href: '/' },
+    { label: 'Leaderboard', href: '/leaderboard' },
+  ];
+  
   return (
-    <div>
-        <h1 className='text-center mt-8 text-2xl'> Leaderboard page</h1>
+
+    <div className="mt-4">
+      <div className='ml-28'>
+         <Breadcrumbs items={breadcrumbs} icon={IoHomeOutline} /> 
+      </div>
+      {/* including filtters */}
+      <div className="flex justify-center items-center mt-4">
+      <Box maxWidth="700px">
+      <Flex gap="1" align="center">
+        <Card>
+          <Flex gap="3" align="center">
+            <RiAwardLine size="32" />
+            <Box>
+              <Text as="div" size="2" weight="bold">
+                My Score
+              </Text>
+              <Text as="div" size="2" color="gray">
+                500
+              </Text>
+            </Box>
+          </Flex>
+        </Card>
+        <Card>
+          <Flex gap="3" align="center">
+            <FaTrophy size="32" />
+            <Box>
+              <Text as="div" size="2" weight="bold">
+                Latest High Score
+              </Text>
+              <Text as="div" size="2" color="gray">
+                1000
+              </Text>
+            </Box>
+          </Flex>
+        </Card>
+        <Card>
+          <Flex gap="3" align="center">
+            <FaTrophy size="32" />
+            <Box>
+              <Text as="div" size="2" weight="bold">
+                Competition
+              </Text>
+              <Text as="div" size="2" color="gray">
+                1st Place
+              </Text>
+            </Box>
+          </Flex>
+        </Card>
+        <Card>
+          <Flex gap="3" align="center">
+            <FaTrophy size="32" />
+            <Box>
+              <Text as="div" size="2" weight="bold">
+                One More Card
+              </Text>
+              <Text as="div" size="2" color="gray">
+                1st Place
+              </Text>
+            </Box>
+          </Flex>
+        </Card>
+      </Flex>
+    </Box>
+      </div>
+      {/* including search bar */}
+      <div className="flex justify-center items-center mt-4">
+      <SegmentedControl.Root defaultValue="inbox">
+  <SegmentedControl.Item value="today">Today</SegmentedControl.Item>
+  <SegmentedControl.Item value="drafts">This Week</SegmentedControl.Item>
+  <SegmentedControl.Item value="inbox">This Month</SegmentedControl.Item>
+  <SegmentedControl.Item value="year">This Year</SegmentedControl.Item>
+</SegmentedControl.Root>
+      </div>
+    <Tables />
     </div>
-  )
+  );
 }
 
-export default Leaderboard
+export default LeaderboardList;
