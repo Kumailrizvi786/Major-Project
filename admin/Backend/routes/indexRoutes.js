e3aae30cd2a66714f75e4244518255c2b1670ff8
Syed Kumail Rizvi <rizvikumail98@gmail.com>
2024-04-02 16:38:10 +0530

    Admin Back End Update

--- admin/Backend/routes/indexRoutes.js
+++ admin/Backend/routes/indexRoutes.js
@@ -1,6 +1,3 @@
-import { checkAuth } from '../middlewares/checkAuth.js';
-
-
 export const routes = (app) => {  
     app.get("/sayHello", (req, res) => {
         res.json({message: "Hello World"});
