c28cc450449e3b1e176797dfc36d72aafe120e9b
Syed Kumail Rizvi <rizvikumail98@gmail.com>
2024-04-02 16:32:48 +0530

    Admin Front End Set Update

--- admin/Frontend/vite.config.js
+++ admin/Frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], root: 'src', server: {port: 3000}
+  plugins: [react()], server: {port: 3000}
 });
