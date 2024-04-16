5d0dfb37cf33bca4124f5e6bd6924865c3829d88
sahilali88084667@gmail.com <sahilali88084667@gmail.com>
2024-04-16 16:55:14 +0530

    Update React Router and TailwindCSS versions

--- admin/Frontend/src/main.jsx
+++ admin/Frontend/src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}  />
   </React.StrictMode>,
 )
