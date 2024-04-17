f0e535e724bd4fc37cdb49e3db4ec208f1446ba4
sahilali88084667@gmail.com <sahilali88084667@gmail.com>
2024-04-17 17:13:52 +0530

    Update icons in Dashboard component

--- admin/Frontend/src/pages/Dashboard.jsx
+++ admin/Frontend/src/pages/Dashboard.jsx
@@ -4,6 +4,8 @@ import { RiAlarmWarningLine, RiHome2Line, RiHourglass2Fill, RiShoppingCart2Line,
 import AllCard from '../components/AllCard';
 import CreateExercise from '../components/Excercise/CreateExcercise';
 import DashboardCard from '../components/DashboardCard';
+import { FiSettings } from 'react-icons/fi';
+import { FaUserAstronaut, FaUserCheck, FaUserDoctor, FaUserGraduate } from 'react-icons/fa6';
 
 function Dashboard() {
      
@@ -28,9 +30,9 @@ function Dashboard() {
                 <AllCard/>
         </div> */}
         <div className="flex flex-wrap">
-          <DashboardCard loading={false} bgColor="#0073B7" icon={<RiHourglass2Fill  />} value="Total Exercise" additionalField={dashData.woTotal} description="Last 30 Days"  />
-          <DashboardCard loading={false} bgColor="#00C0EF" icon={<RiShoppingCart2Line  />} value="Registered User" additionalField={dashData.woGenerated} description="Last 30 Days" />
-          <DashboardCard loading={false} bgColor="#F39C12" icon={<RiTruckLine  />} value="Top Users" additionalField={dashData.woInTransit} description="Last 30 Days"  />
+          <DashboardCard loading={false} bgColor="#0073B7" icon={<FiSettings  />} value="Total Exercise" additionalField={dashData.woTotal} description="Last 30 Days"  />
+          <DashboardCard loading={false} bgColor="#00C0EF" icon={<FaUserAstronaut  />} value="Registered User" additionalField={dashData.woGenerated} description="Last 30 Days" />
+          <DashboardCard loading={false} bgColor="#F39C12" icon={<FaUserGraduate  />} value="Top Users" additionalField={dashData.woInTransit} description="Last 30 Days"  />
           <DashboardCard loading={false} bgColor="#00A65A" icon={<RiAlarmWarningLine  />} value="Total Etc." additionalField={dashData.woWaitingForApproval} description="Last 30 Days"  />
         </div>
         <CreateExercise/>
