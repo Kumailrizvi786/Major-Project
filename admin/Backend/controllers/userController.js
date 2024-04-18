45a41867cb8b76e6897f85220b03091f476d7b13
Syed Kumail Rizvi <rizvikumail98@gmail.com>
2024-04-18 22:07:52 +0530

    Get All User API in Admin Added

---
+++ admin/Backend/controllers/userController.js
@@ -0,0 +1,15 @@
+import User from '../models/UserModel.js';
+
+
+export const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find().populate('role');
+        if(!users){
+            return res.status(404).json({message: 'No Record found'})
+        }
+        return res.status(200).json(users);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: "Internal Server Error " + err.message });
+    }
+}
--- admin/Backend/routes/indexRoutes.js
+++ admin/Backend/routes/indexRoutes.js
@@ -1,7 +1,9 @@
 import exerciseRoutes from './exerciseRoutes.js';
 import adminRoutes from './adminRoutes.js';
+import userRoutes from './userRoutes.js';
 
 export const routes = (app) => {
     app.use('/admin/exercise', exerciseRoutes);
     app.use('/admin', adminRoutes);
+    app.use('/admin/user', userRoutes);
 }
