7338c79ab0fdde3b2df7bdc2567c77a8840cb91f
Syed Kumail Rizvi <rizvikumail98@gmail.com>
2024-04-18 22:36:38 +0530

    Slight Change in UserController in Admin

--- admin/Backend/controllers/userController.js
+++ admin/Backend/controllers/userController.js
@@ -4,9 +4,12 @@ import User from '../models/UserModel.js';
 export const getAllUsers = async (req, res) => {
     try {
         const users = await User.find().populate('role');
-        if(!users){
-            return res.status(404).json({message: 'No Record found'})
+        if (!users) {
+            return res.status(404).json({ message: 'No Record found' })
         }
+        users.forEach(user => {
+            user.password = undefined;
+        });
         return res.status(200).json(users);
     } catch (err) {
         console.log(err);
