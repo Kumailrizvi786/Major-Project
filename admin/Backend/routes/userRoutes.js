f1706b49ad661986e33b4b021e002e56e9b4f05e
Syed Kumail Rizvi <rizvikumail98@gmail.com>
2024-04-19 10:44:28 +0530

    Admin BackEnd: UserController -> Add Update User API

--- admin/Backend/routes/userRoutes.js
+++ admin/Backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {getAllUsers, createUser} from '../controllers/userController.js'
+import {getAllUsers, createUser, updateUser} from '../controllers/userController.js'
 
 const router = express.Router();
 
 router.get('/getAll', getAllUsers);
 router.post('/create', createUser);
+router.put('/update', updateUser);
 
 export default router;
