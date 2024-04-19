96508fc80566bf8fea05fc2ac3901afea3e1fcea
Syed Kumail Rizvi <rizvikumail98@gmail.com>
2024-04-19 11:23:54 +0530

    Admin BackEnd: UserController -> Add Get User By Email

--- admin/Backend/controllers/userController.js
+++ admin/Backend/controllers/userController.js
@@ -3,6 +3,22 @@ import Role from '../models/RoleModel.js';
 import bcrypt from 'bcryptjs';
 
 
+export const getUserByEmail = async (req,res) => {
+    try{
+        const {email} = req.params;
+        const user = await User.findOne({email: email});
+        if(!user){
+            return res.status(404).json({message: "User not found"});
+        }
+        user.password = undefined;
+
+        return res.status(200).json(user);
+    }catch(err){
+        console.log(err);
+        return res.status(500).json({message: "Internal Server Error"});
+    }
+}
+
 export const getAllUsers = async (req, res) => {
     try {
         const users = await User.find().populate('role');
--- admin/Backend/routes/userRoutes.js
+++ admin/Backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {getAllUsers, createUser, updateUser, deleteUser} from '../controllers/userController.js'
+import {getAllUsers, createUser, updateUser, deleteUser, getUserByEmail} from '../controllers/userController.js'
 
 const router = express.Router();
 
 router.post('/create', createUser);
 router.get('/getAll', getAllUsers);
+router.get('/get/:email', getUserByEmail);
 router.put('/update', updateUser);
 router.delete('/delete', deleteUser);
 
