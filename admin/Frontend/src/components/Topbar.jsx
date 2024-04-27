85fb695027bdaed396cd3c882d024d600737020b
sahilali88084667@gmail.com <sahilali88084667@gmail.com>
2024-04-28 00:05:26 +0530

    Fix typo in Topbar.jsx

--- admin/Frontend/src/components/Topbar.jsx
+++ admin/Frontend/src/components/Topbar.jsx
@@ -13,7 +13,7 @@ const TopBar = ({open}) => {
              
               <IoSpeedometerOutline className="ml-2 mb-1 text-[#3859C7] mr-2 inline-block" size={20} />   
 <div className={`relative inline-block ${open ? "mt-3 left-[0px]" : "-left-[190px]"} duration-300`}>
-    <div className='text-md font-medium text-[#3859C7] duration-150'>Reed For Speed</div>
+    <div className='text-md font-medium text-[#3859C7] duration-150'>Read For Speed</div>
 </div>
 
 
