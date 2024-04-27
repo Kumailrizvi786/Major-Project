486b2ae7e73e3364f3fe14870d481688f8d14ee6
sahilali88084667@gmail.com <sahilali88084667@gmail.com>
2024-04-27 23:04:20 +0530

    Update Usermanagement.jsx to fetch and display all users from the backend API

--- admin/Frontend/src/pages/Usermanagement.jsx
+++ admin/Frontend/src/pages/Usermanagement.jsx
@@ -1,22 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Breadcrumbs from '../components/Breadcrumbs';
 import { RiAdminLine, RiHome2Line } from 'react-icons/ri';
-import { Button, Table, TextField } from '@radix-ui/themes';
+import { Badge, Button, Table, TextField } from '@radix-ui/themes';
 import { MagnifyingGlassIcon, TrashIcon } from '@radix-ui/react-icons';
 import { RxAllSides } from 'react-icons/rx';
 import { FaUserGroup } from 'react-icons/fa6';
 import { IoAdd, IoAddSharp, IoRemove } from 'react-icons/io5';
 import { FaUser } from 'react-icons/fa';
 import { FiDelete } from 'react-icons/fi';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
 
 function Usermanagement() {
-  const users = [
-    { id: 1, name: 'John Doe', email: 'john@example.com', role: 'Admin' },
-    { id: 2, name: 'Jane Smith', email: 'jane@example.com', role: 'User' },
-    { id: 3, name: 'Alice Johnson', email: 'alice@example.com', role: 'User' },
-    { id: 4, name: 'Bob Brown', email: 'bob@example.com', role: 'User' },
-    { id: 5, name: 'Eva White', email: 'eva@example.com', role: 'User' },
-  ];
+  const [loading, setLoading] = useState(false);
+  const [Allusers, setAllusers] = useState([]);
+  
+
+  const getAllUsers = async () => {
+    const url = 'http://localhost:8000/admin/user/getAll';  
+    try{
+      setLoading(true);
+      const response = await axios.get(url);
+      console.log(response.data);
+      setAllusers(response.data);
+      setLoading(false);
+
+    }catch(error){
+      setLoading(false);
+      console.error(error);
+      toast.error("Unable to fetch data!")
+    }
+  }
+
+  useEffect(()=>{
+    // const users = 
+    getAllUsers();
+  },[])
+ 
     const breadcrumbsItems = [
         { text: 'Home', link: '/', icon: <RiHome2Line /> },
         { text: 'User Management' },
@@ -54,32 +74,27 @@ function Usermanagement() {
     <Table.Row>
       <Table.ColumnHeaderCell>Full name</Table.ColumnHeaderCell>
       <Table.ColumnHeaderCell>Email</Table.ColumnHeaderCell>
+      <Table.ColumnHeaderCell>Verified Status</Table.ColumnHeaderCell>
       <Table.ColumnHeaderCell>Role</Table.ColumnHeaderCell>
       <Table.ColumnHeaderCell>Action</Table.ColumnHeaderCell>
     </Table.Row>
   </Table.Header>
 
   <Table.Body>
+ { Allusers.map((user) => (
     <Table.Row>
-      <Table.RowHeaderCell>Sahil Ali</Table.RowHeaderCell>
-      <Table.Cell>danilo@example.com</Table.Cell>
-      <Table.Cell>Developer</Table.Cell>
-      <Table.Cell><Button className='bg-red-600'>Remove User <TrashIcon/> </Button></Table.Cell>
+      <Table.RowHeaderCell>{user.name}</Table.RowHeaderCell>
+      <Table.Cell>{user.email}</Table.Cell>
+      <Table.Cell><Badge radius="full" color={user.isEmailVerified ? "blue": "red"}>{user.isEmailVerified ? "Verified User": "Not Verified"}</Badge></Table.Cell>
+      <Table.Cell>{user.role.name}</Table.Cell>
+      <Table.Cell><Button className={user.role.name !== "admin" && 'bg-red-600'} disabled={user.role.name == "admin"}>Remove User <TrashIcon/> </Button></Table.Cell>
     </Table.Row>
+  ))}
+  
 
-    <Table.Row>
-      <Table.RowHeaderCell>Syed Kumail Rizvi</Table.RowHeaderCell>
-      <Table.Cell>zahra@example.com</Table.Cell>
-      <Table.Cell>Admin</Table.Cell>
-      <Table.Cell><Button className='bg-red-600 rounded-md'>Remove User  <TrashIcon/> </Button></Table.Cell>
-    </Table.Row>
+   \
 
-    <Table.Row>
-      <Table.RowHeaderCell>Mohd Maaz</Table.RowHeaderCell>
-      <Table.Cell>jasper@example.com</Table.Cell>
-      <Table.Cell>Developer</Table.Cell>
-      <Table.Cell><Button className='bg-red-600'>Remove User <TrashIcon/> </Button></Table.Cell>
-    </Table.Row>
+    
   </Table.Body>
 </Table.Root>
         </div>
