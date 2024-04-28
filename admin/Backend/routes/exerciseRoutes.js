4f4d193b3522be62a52db14c7eb6255a13460a8d
Syed Kumail Rizvi <rizvikumail98@gmail.com>
2024-04-29 00:26:26 +0530

    Add Update Exercise by Id

--- admin/Backend/routes/exerciseRoutes.js
+++ admin/Backend/routes/exerciseRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import {getExerciseById, createExercise, updateExerciseByName, deleteExerciseById, getAllExercise} from "../controllers/exerciseController.js";
+import {getExerciseById, createExercise, updateExerciseById, deleteExerciseById, getAllExercise} from "../controllers/exerciseController.js";
 
 const router = express.Router();
 
 router.get('/getAll', getAllExercise);
 router.get('/getById/:id', getExerciseById);
 router.post('/create', createExercise);
-router.put('/updateByName', updateExerciseByName);
+router.put('/updateById', updateExerciseById);
 router.delete('/deleteById/:id', deleteExerciseById);
 
 export default router;
